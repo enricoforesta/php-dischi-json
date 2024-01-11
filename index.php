28c6bf1fd666677703420c260bd4e7946f027336
enricoforesta <enrico.foresta97@gmail.com>
2024-01-11 15:24:45 +0100

    feat:scaffolding

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- /MY CSS -->
+</head>
+
+<body>
+    <div id="app">
+        <h1>ciao</h1>
+    </div>
+
+
+
+    <!-- VUE -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- /VUE -->
+    <!-- AXIOS -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- /AXIOS -->
+    <!-- MY JS -->
+    <script src="./js/index.js"></script>
+    <!-- /MY JS -->
+</body>
+
+</html>
